afa150cd2d14c14493f4427c415d25d6185b974a
amelakor <amela_korjenic@hotmail.com>
2023-06-23 15:36:52 +0200

    Categories UI

--- src/app/components/Navbar/Logo.tsx
+++ src/app/components/Navbar/Logo.tsx
@@ -6,6 +6,7 @@ const Logo = () => {
     const router = useRouter();
     return (
         <Image
+            onClick={() => router.push("/")}
             alt="logo"
             src="/images/logo.png"
             width={100}
